71ca8d6fd5b4bd87a9baf4e260c664257324a7fb
Francis Chen <fncis.chen@gmail.com>
2019-03-18 14:38:02 -0700

    added endpoints for server requests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // implement your API here
+const express = require('express');
+const server = express(); // creates server for API calls :)
+
+server.get('/api/users', (req, res) => {
+}
+
+server.post('/api/users', (req, res) => {
+}
+
+server.get('/api/users/:id', (req, res) => {
+}
+
+server.delete('/api/users/:id', (req, res) => {
+}
+
+server.put('/api/users/:id', (req, res) => {
+}
+
