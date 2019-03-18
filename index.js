190434be57fbae6e9515cdbe99652f20940e3246
Francis Chen <fncis.chen@gmail.com>
2019-03-18 15:01:37 -0700

    added post method

--- index.js
+++ index.js
@@ -3,16 +3,28 @@ const express = require('express');
 const server = express(); // creates server for API calls :)
 
 server.get('/api/users', (req, res) => {
-}
+    if(!req.body) {
+        res.status(500).send("error: The users information could not be retrieved.")
+    }
+    else{
+        res.json(req.body);
+    }
+});
 
 server.post('/api/users', (req, res) => {
-}
+    if(req.body.name == null | req.body.bio == null) {
+        res.status(400).send("errorMessage: Please provide name and bio for the user.")
+    }
+    else{
+       res.status(201).json()
+    }
+});
 
 server.get('/api/users/:id', (req, res) => {
-}
+});
 
 server.delete('/api/users/:id', (req, res) => {
-}
+});
 
 server.put('/api/users/:id', (req, res) => {
 }
